7fcf6bc178e8a7350fa0ff6104ab692bb8118453
SanketModhe <113822048+SanketModhe@users.noreply.github.com>
2022-09-22 18:56:08 +0530

    Create class.js

---
+++ class.js
@@ -0,0 +1,37 @@
+//number,boolean,array,object
+//undefined datatype (new)
+//set map
+
+
+//program 1 (object literal)
+
+let Ajit = {
+    fisrtName:"Ajit",
+    lastName:"Gode",
+    age:23,
+    skill:["python","Angular","HTML"]
+}
+
+let Sanket = {
+    fisrtName:"Sanket",
+    lastName:"Modhe",
+    age:23,
+    skill:["python","Angular","java"]
+    
+}
+
+
+//Template  ---Blue Print
+//User Defined Datatype --- class
+
+class Person{
+    firstName;
+    lastName;
+    age;
+    skills=[];
+}
+
+let Ajit2 = new Person()
+
+let Sanket2 = new Person()
+console.log(Ajit2)
